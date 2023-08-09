eef8f26f3f96e5e6eab82af952b75ac7eeb0c810
Daniel De Jesus Tejeda Pena <daniel.tejeda@vasslatam.com>
2023-08-09 12:26:38 -0500

    wip: maquetacion

--- src/ui/components/Inputs/TextInput.tsx
+++ src/ui/components/Inputs/TextInput.tsx
@@ -13,7 +13,7 @@ const MyTextInput = ({ label, ...props }: Props) => {
   const [field] = useField(props);
 
   return (
-    <div className='flex flex-coll'>
+    <div className='flex flex-col'>
       {label && <label htmlFor={props.id || props.name} className={props.labelstyle ? props.labelstyle : "font-bold inline w-auto"}>{label}</label>}
       <div className='inline w-full'>
         <input type="text" id={props.name} className='border border-black-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 w-full' {...field} {...props}/>
